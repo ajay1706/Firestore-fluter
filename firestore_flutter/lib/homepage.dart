3c2f73c4aac8af15b7f96506a259839689eb4448
Henri Mäkelä <henri.makela@boogiesoftware.com>
2019-10-17 18:19:39 +0300

    add search functionality

--- firestore_flutter/lib/homepage.dart
+++ firestore_flutter/lib/homepage.dart
@@ -12,43 +12,41 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
 //For TopPost
-StreamSubscription<QuerySnapshot> subscription;
+  StreamSubscription<QuerySnapshot> subscription;
 
-List<DocumentSnapshot> snapshot;
+  List<DocumentSnapshot> snapshot;
 
-CollectionReference collectionReference = Firestore.instance.collection("TopPost");
+  CollectionReference collectionReference =
+      Firestore.instance.collection("TopPost");
 
 //For body post
-StreamSubscription<QuerySnapshot> sdSubscription;
-List<DocumentSnapshot> sdSnapshot;
-CollectionReference sdCollectionReference = Firestore.instance.collection("BodyPost");
+  StreamSubscription<QuerySnapshot> sdSubscription;
+  List<DocumentSnapshot> sdSnapshot;
+  CollectionReference sdCollectionReference =
+      Firestore.instance.collection("BodyPost");
 
-
-
-@override
-  void initState() {   
-    subscription  = collectionReference.snapshots().listen((dataSnapshot){
-setState(() {
-  snapshot = dataSnapshot.documents;
-});
+  @override
+  void initState() {
+    subscription = collectionReference.snapshots().listen((dataSnapshot) {
+      setState(() {
+        snapshot = dataSnapshot.documents;
+      });
     });
-sdSubscription = sdCollectionReference.snapshots().listen((sdDataSnapshot){
-  setState(() {
-    sdSnapshot = sdDataSnapshot.documents;
-  });
+    sdSubscription = sdCollectionReference.snapshots().listen((sdDataSnapshot) {
+      setState(() {
+        sdSnapshot = sdDataSnapshot.documents;
+      });
+    });
 
-
-});
-
-      super.initState();
+    super.initState();
   }
 
-  passData(DocumentSnapshot snap){
-    Navigator.of(context).push(MaterialPageRoute(builder:(context) => DetailPage(snapshot: snap,)));
+  passData(DocumentSnapshot snap) {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => DetailPage(
+              snapshot: snap,
+            )));
   }
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -59,9 +57,11 @@ sdSubscription = sdCollectionReference.snapshots().listen((sdDataSnapshot){
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.search),
-            onPressed: () => debugPrint("Search"),
+            onPressed: () {
+              showSearch(context: context, delegate: ItemSearch(sdSnapshot));
+            },
           ),
-             IconButton(
+          IconButton(
             icon: Icon(Icons.add),
             onPressed: () => debugPrint("Add"),
           )
@@ -70,35 +70,30 @@ sdSubscription = sdCollectionReference.snapshots().listen((sdDataSnapshot){
       drawer: Drawer(
         child: ListView(
           children: <Widget>[
-
-
             UserAccountsDrawerHeader(
               accountName: Text("Ajay"),
-              accountEmail:Text("xyz123@gmail.com") ,
-              decoration: BoxDecoration(
-                color: Colors.redAccent
-              ),
+              accountEmail: Text("xyz123@gmail.com"),
+              decoration: BoxDecoration(color: Colors.redAccent),
             ),
-
             ListTile(
               title: Text("First Page"),
-              leading: Icon(Icons.search,color:Colors.green),
+              leading: Icon(Icons.search, color: Colors.green),
             ),
-              ListTile(
+            ListTile(
               title: Text("Second Page"),
-              leading: Icon(Icons.add,color:Colors.blueGrey),
+              leading: Icon(Icons.add, color: Colors.blueGrey),
             ),
-              ListTile(
+            ListTile(
               title: Text("Third Page"),
-              leading: Icon(Icons.help,color:Colors.redAccent),
+              leading: Icon(Icons.help, color: Colors.redAccent),
             ),
             Divider(
               height: 10,
               color: Colors.grey,
             ),
-              ListTile(
+            ListTile(
               title: Text("Close "),
-              trailing: Icon(Icons.close,color:Colors.red),
+              trailing: Icon(Icons.close, color: Colors.red),
               onTap: () {
                 Navigator.of(context).pop();
               },
@@ -106,127 +101,254 @@ sdSubscription = sdCollectionReference.snapshots().listen((sdDataSnapshot){
           ],
         ),
       ),
-      
-body: ListView(
-  children: <Widget>[
-    Container(
-
-      height: 200,
-      child: ListView.builder(
-        itemCount: snapshot.length,
-        scrollDirection: Axis.horizontal,
-        itemBuilder: (context, index){
-          return Card(
-            margin: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-            elevation: 10,
-            child: Container(
-              margin: EdgeInsets.all(10),
-              child: Column(
-                children: <Widget>[
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(snapshot[index].data['url'],
-                    height:120,
-                    width: 120,
-                    fit:BoxFit.cover,
-
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 200,
+            child: ListView.builder(
+              itemCount: snapshot.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Card(
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                  elevation: 10,
+                  child: Container(
+                    margin: EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            snapshot[index].data['url'],
+                            height: 120,
+                            width: 120,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          snapshot[index].data['title'],
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    snapshot[index].data['title'],
-                    style: TextStyle(color: Colors.red,
-                    fontSize: 18,
-                    ),
-
-                  )
-                ],
-              ),
+                );
+              },
             ),
-          );
-
-
-        },
+          ),
+          Container(
+            height: MediaQuery.of(context).size.height,
+            child: ListView.builder(
+              itemCount: sdSnapshot.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 8,
+                  margin: EdgeInsets.all(10),
+                  child: Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            CircleAvatar(
+                              child: Text(sdSnapshot[index].data['title'][0]),
+                              backgroundColor: Colors.redAccent,
+                              foregroundColor: Colors.white,
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            InkWell(
+                              child: Text(
+                                sdSnapshot[index].data['title'],
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.black),
+                              ),
+                              onTap: () => passData(sdSnapshot[index]),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Hero(
+                              tag: sdSnapshot[index].data['url'],
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15),
+                                child: Image.network(
+                                  sdSnapshot[index].data['url'],
+                                  height: 180,
+                                  width: MediaQuery.of(context).size.width,
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.thumb_up,
+                                  color: Colors.green,
+                                ),
+                                Icon(
+                                  Icons.share,
+                                  color: Colors.blue,
+                                ),
+                                Icon(
+                                  Icons.thumb_down,
+                                  color: Colors.red,
+                                )
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
       ),
+    );
+  }
+}
 
+class ItemSearch extends SearchDelegate<List<DocumentSnapshot>> {
+  List<DocumentSnapshot> items;
 
-    ),
+  ItemSearch(this.items);
 
-    Container(
-        height: MediaQuery.of(context).size.height,
-       child: ListView.builder(
-         itemCount: sdSnapshot.length,
-         itemBuilder: (context, index){
-     return Card(
-       elevation: 8,
-       margin: EdgeInsets.all(10),
-       child: Container(
-         padding: EdgeInsets.all(10),
-         child: Column(
-           children: <Widget>[
-             Row(
-               children: <Widget>[
-                 CircleAvatar(
-                   
-                   child: Text(sdSnapshot[index].data['title'][0]),
-                   backgroundColor: Colors.redAccent,
-                   foregroundColor: Colors.white,
-                   
-                 ),
-                 SizedBox(width: 10,),
-                 InkWell(
-                                    child: Text(sdSnapshot[index].data['title'],
-                   style: TextStyle(fontSize: 20,color:Colors.black),),
-                 onTap: () => passData(sdSnapshot[index]),
-                 
-                 )
-
-                
-               ],
-             ),
-             SizedBox(
-               height:10 ,
-             ),
-             Column(
-               children: <Widget>[
-               Hero(
-                 tag: sdSnapshot[index].data['url'],
-                 child: ClipRRect(
-
-                   borderRadius: BorderRadius.circular(15),
-                   child: Image.network(sdSnapshot[index].data['url'],
-                   height: 180,
-                   width: MediaQuery.of(context).size.width,
-                   fit: BoxFit.cover,),
-
-                 ),
-               ),
-               SizedBox(height: 10,),
-               Row(
-                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                 children: <Widget>[
-                   Icon(Icons.thumb_up,color: Colors.green,),
-            
-                   Icon(Icons.share,color: Colors.blue,),
-             
-                   Icon(Icons.thumb_down,color: Colors.red,)
-                 ],
-               )
-               ],
-             )
-           ],
-         ),
-       ),
-
-     );
-         },
-       ),
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: Icon(Icons.clear),
+        onPressed: () {
+          query = "";
+        },
       )
-  ],
-),
+    ];
+  }
 
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return Container();
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final results =
+        items.where((a) => a['title'].toLowerCase().contains(query)).toList();
+
+    return ListView.builder(
+        itemCount: results.length,
+        itemBuilder: (context, index) {
+          DocumentSnapshot snapshot = results[index];
+          return CustomCard(snapshot);
+        });
+  }
+}
+
+class CustomCard extends StatelessWidget {
+  final DocumentSnapshot snapshot;
+
+  CustomCard(this.snapshot);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 8,
+      margin: EdgeInsets.all(10),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                CircleAvatar(
+                  child: Text(snapshot.data['title'][0]),
+                  backgroundColor: Colors.redAccent,
+                  foregroundColor: Colors.white,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                InkWell(
+                    child: Text(
+                      snapshot.data['title'],
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                    onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => DetailPage(
+                              snapshot: snapshot,
+                            ))))
+              ],
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Column(
+              children: <Widget>[
+                Hero(
+                  tag: snapshot.data['url'],
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.network(
+                      snapshot.data['url'],
+                      height: 180,
+                      width: MediaQuery.of(context).size.width,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(
+                      Icons.thumb_up,
+                      color: Colors.green,
+                    ),
+                    Icon(
+                      Icons.share,
+                      color: Colors.blue,
+                    ),
+                    Icon(
+                      Icons.thumb_down,
+                      color: Colors.red,
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
